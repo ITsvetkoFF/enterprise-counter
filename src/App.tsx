a767c6bea50116ce98a1aa414239779c7af181da
itsvetkoff <iotsvetkov@gmail.com>
2022-12-09 14:27:39 +0200

    add multiple counters and immer

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,66 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+
+import produce from "immer";
+
+type SingleCounterState = {
+  type: "incrementing" | "decrementing";
+  value: number;
+};
+
+type AppState = {
+  counters: SingleCounterState[];
+};
+
+const initialState: AppState = {
+  counters: [
+    {
+      type: "incrementing",
+      value: 0,
+    },
+  ],
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [state, setState] = useState(initialState);
 
   return (
     <div className="App">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <div className="CounterGrid">
+        {state.counters.map((counter, index) => (
+          <button
+            onClick={() =>
+              setState((prevState) =>
+                produce(prevState, (draft) => {
+                  draft.counters[index].value++;
+                })
+              )
+            }
+          >
+            count is {state.counters[index].value}
+          </button>
+        ))}
+      </div>
+      <section>
+        <h2>Configure counters</h2>
+        <button
+          onClick={() => {
+            setState((prevState) => ({
+              counters: [
+                ...prevState.counters,
+                {
+                  type: "incrementing",
+                  value: 0,
+                },
+              ],
+            }));
+          }}
+        >
+          Add incrementing counter
         </button>
+      </section>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
